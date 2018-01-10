d67637b5c4aacef43e1153ce603ca7333196369f
edewajosh <joshedewa@gmail.com>
2018-01-10 16:52:55 +0300

    More changes made

--- posts/admin.py
+++ posts/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Posts
 
 class PostsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title',"author", 'created_at')
 
 admin.site.register(Posts, PostsAdmin)
--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class Posts(models.Model):
   title = models.CharField(max_length=200)
   body = models.TextField()
+  author = models.CharField(max_length=200)
   created_at = models.DateTimeField(default=datetime.now, blank=True)
   def __str__(self):
     return self.title
