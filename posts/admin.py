642777593c591710c4a698a21a6e22d82d0bccdb
edewajosh <joshedewa@gmail.com>
2018-01-11 09:46:45 +0300

    Added a comment model

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Posts
+from .models import Posts, Comments
 
 class PostsAdmin(admin.ModelAdmin):
     list_display = ('title',"author", 'created_at')
 
 admin.site.register(Posts, PostsAdmin)
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('text','post', 'pub_date')
+
+admin.site.register(Comments, CommentsAdmin)
--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,20 @@ class Posts(models.Model):
   body = models.TextField()
   author = models.CharField(max_length=200)
   created_at = models.DateTimeField(default=datetime.now, blank=True)
+
   def __str__(self):
     return self.title
+
   class Meta:
     verbose_name_plural = "Posts"
+
+class Comments(models.Model):
+  post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+  text = models.TextField()
+  pub_date = models.DateTimeField(default = datetime.now, blank=True)
+
+  def __str__(self):
+    return self.text
+
+  class Meta:
+    verbose_name_plural = "Comments"
