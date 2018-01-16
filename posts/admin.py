56a0b90a056882cc7f039eb1a7049997fbaffa0c
edewajosh <joshedewa@gmail.com>
2018-01-16 13:17:51 +0300

    fixed posts admin

--- posts/admin.py
+++ posts/admin.py
@@ -10,9 +10,9 @@ admin.site.register(Posts, PostsAdmin)
 
 class CommentsAdmin(admin.ModelAdmin):
     list_display = ('text','post', 'pub_date')
-<<<<<<< HEAD
+#<<<<<<< HEAD
     #field = ('text')
-=======
->>>>>>> 642777593c591710c4a698a21a6e22d82d0bccdb
+#=======
+#>>>>>>> 642777593c591710c4a698a21a6e22d82d0bccdb
 
 admin.site.register(Comments, CommentsAdmin)
